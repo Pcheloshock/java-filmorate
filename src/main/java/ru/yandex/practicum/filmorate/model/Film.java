d118e2690e0d278c36c13f4345b875509a866ec3
pcheloshock <kraniys@yandex.ru>
2025-11-16 21:13:17 +0500

    Revert "ФильмоЛаб"
    This reverts commit 248d9d51ef127e0b8b7f184102b4cbc193425864.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,22 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
-import jakarta.validation.constraints.*;
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+/**
+ * Film.
+ */
+@Getter
+@Setter
 public class Film {
-    private int id;
-
-    @NotBlank(message = "Название не может быть пустым")
-    private String name;
-
-    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
-    private String description;
-
-    @NotNull(message = "Дата релиза обязательна")
-    private LocalDate releaseDate;
-
-    @Positive(message = "Продолжительность должна быть положительным числом")
-    private int duration;
 }
