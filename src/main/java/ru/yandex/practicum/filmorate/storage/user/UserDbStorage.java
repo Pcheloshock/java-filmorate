2371bf321bb37c0aff4b0f8848ad1bb0d258d2bd
pcheloshock <kraniys@yandex.ru>
2025-12-24 16:01:39 +0500

    FilmLab1.5.1

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
