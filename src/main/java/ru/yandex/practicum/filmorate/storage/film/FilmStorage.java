a28de5a3ca14d8019df5a4b3c3bab33877b7e868
pcheloshock <kraniys@yandex.ru>
2025-12-24 15:16:30 +0500

    FilmLab1.5.1

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,17 +1,26 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
     Film create(Film film);
+
     Film update(Film film);
+
     List<Film> findAll();
+
     Optional<Film> findById(int id);
+
     void delete(int id);
+
     boolean existsById(int id);
+
     void addLike(int filmId, int userId);
+
     void removeLike(int filmId, int userId);
+
     List<Film> getPopularFilms(int count);
 }
