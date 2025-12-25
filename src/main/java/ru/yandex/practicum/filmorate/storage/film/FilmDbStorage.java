d7c6c942b165f6609540650ddc4d3df7d8175add
pcheloshock <kraniys@yandex.ru>
2025-12-25 21:22:30 +0500

    FilmLab1.5.1

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -13,6 +13,7 @@ import ru.yandex.practicum.filmorate.model.MpaRating;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
