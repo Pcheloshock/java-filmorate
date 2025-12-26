593ba75fc8436dd821d3f477ce69da03b7f4a51c
pcheloshock <kraniys@yandex.ru>
2025-12-26 22:55:41 +0500

    FilmLab1.5.1

--- src/test/java/ru/yandex/practicum/filmorate/ControllerTestConfig.java
+++ src/test/java/ru/yandex/practicum/filmorate/ControllerTestConfig.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import ru.yandex.practicum.filmorate.storage.film.*;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
@@ -13,19 +12,16 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 public class ControllerTestConfig {
 
     @Bean
-    //@Primary
     public FilmStorage inMemoryFilmStorage() {
         return new InMemoryFilmStorage();
     }
 
     @Bean
-    //@Primary
     public UserStorage inMemoryUserStorage() {
         return new InMemoryUserStorage();
     }
 
     @Bean
-    //@Primary
     public GenreMpaStorage inMemoryGenreMpaStorage() {
         return new InMemoryGenreMpaStorage();
     }
