38c6d6b85c17dd11d0ddd6183db70b6a67bcd958
pcheloshock <kraniys@yandex.ru>
2025-12-27 19:03:05 +0500

    FilmLab1.5.1

--- src/test/java/ru/yandex/practicum/filmorate/TestConfig.java
+++ src/test/java/ru/yandex/practicum/filmorate/TestConfig.java
@@ -3,15 +3,47 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import ru.yandex.practicum.filmorate.storage.film.GenreMpaStorage;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryGenreMpaStorage;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import ru.yandex.practicum.filmorate.storage.film.*;
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import javax.sql.DataSource;
 
 @TestConfiguration
 public class TestConfig {
 
     @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
     @Primary
-    public GenreMpaStorage genreMpaStorage() {
-        return new InMemoryGenreMpaStorage();
+    public FilmStorage filmStorage(JdbcTemplate jdbcTemplate) {
+        return new FilmDbStorage(jdbcTemplate);
+    }
+
+    @Bean
+    @Primary
+    public UserStorage userStorage(JdbcTemplate jdbcTemplate) {
+        return new UserDbStorage(jdbcTemplate);
+    }
+
+    @Bean
+    @Primary
+    public GenreMpaStorage genreMpaStorage(JdbcTemplate jdbcTemplate) {
+        return new GenreMpaDbStorage(jdbcTemplate);
     }
 }
