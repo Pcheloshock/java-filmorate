c6a878e7a33a84a2b77a101dda3ffd7bd05328cd
pcheloshock <kraniys@yandex.ru>
2025-12-27 22:03:49 +0500

    FilmLab1.5.1

--- src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.util.HashMap;
